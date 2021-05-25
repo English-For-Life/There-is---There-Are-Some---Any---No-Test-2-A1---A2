b8ee86b158faee73f73941523840ae135a2cf4c4
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-25 09:57:08 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+    {
+        q:"There is ...... very good hairdresser in our neighbourhood and ...... in our apartment block likes him.",
+        options:["a / everybody", "some / anywhere", "a few / somebody", "no / nobody"],
+        answer: 0
+    },
+    {
+        q:"A: Can I have ...... coffee? B: Sure. Would you like ...... milk in it?",
+        options:["a few / a few", "some / some", "any /no", "no/no"],
+        answer: 1
+    },
+    {
+        q:"A: Is ...... ready for the film? B: Yes, but ...... wants to watch a horror film. They all prefer a comedy.",
+        options:["somebody / everybody", "anywhere / somebody", "everybody / nobody", "something / anyone"],
+        answer: 2
+    },
+    {
+        q:"A: There isn't ...... sugar in this cake. B: But it is very tasty. Is there ...... fruit juice in it? A: Yes.",
+        options:["a little / no", "a / some", "some / any", "any / any"],
+        answer: 3
+    },
+    {
+        q:"A: Can you give me ...... eggs? B: What will you do with them? A: I will make ...... omelette.",
+        options:["some / an", "no / some", "any / a few", "a little / no"],
+        answer: 0
+    },
+    {
+        q:"A: There is ...... park near my house. B: That is bad. Is there ...... else to go for a walk? A: The beach isn't very far. I sometimes go to there and have long walks.",
+        options:["any / nowhere", "no / anywhere", "some / everywhere", "a few / somewhere"],
+        answer: 1
+    },
+    {
+     q:"Don't believe ...... in TV programmes. People on TV can give ...... false information sometimes.",
+        options:["anything / no", "something / any", "everything / some", "nothing / a few"],
+        answer: 2   
+    },
+    {
+        q:"There ...... in the fridge. Let's go shopping.",
+        options:["isn't / everything", "isn't / something", "is / anything", "is / nothing"],
+        answer: 3
+    },
+    {
+        q:"A: Would you like ...... salad? B: No, thanks. I'm so full. I can't eat ...... now.",
+        options:["some / anything", "no / everything", "any / something", "a little / nothing"],
+        answer: 0
+    },
+    {
+        q:"I can't go ...... in summer. I'm so busy and have got ...... time.",
+        options:["somewhere / any", "anywhere / no", "everywhere / some", "nowhere / a little"],
+        answer: 1
+    }
+]
